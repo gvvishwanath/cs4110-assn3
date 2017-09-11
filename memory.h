a7e44264a73c8fe24b7b00caa87cd83149308867
G V Vishwanath <gvvishwanath@gmail.com>
2017-09-11 09:42:02 +0530

    Added function headers for memory operations: memory.h
    Signed-off-by: G V Vishwanath <gvvishwanath@gmail.com>

---
+++ memory.h
@@ -0,0 +1,22 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "instruction.h"
+
+#define CODE_START 0
+#define CODE_END 511
+#define DATA_START 512
+#define DATA_END 1023
+#define MEM_SIZE 1024
+
+char memory[MEM_SIZE];
+
+void loadBinary (char *binary);
+
+void loadValue (short int location, short int nBytes);
+void storeValue (short int location, short int nBytes, short int value);
+
+short int getLabel (InstructionType instr);
+void setLabel (InstructionType instr, char *label, char *binary);
+
+#endif
