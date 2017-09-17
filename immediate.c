bfe1c6859044d32db089f050800ca054a6a943f3
G V Vishwanath <gvvishwanath@gmail.com>
2017-09-17 21:12:42 +0530

    immediate.c: Included stdio.h

--- immediate.c
+++ immediate.c
@@ -1,20 +1,28 @@
 #include "immediate.h"
+#include <stdio.h>
 #include <stdib.h>
 
 short int getImmOp (InstructionType instr) {
-	int shift = CSIZE + ISIZE + OP1SIZE + OP2SIZE;
-	short int immOp = (instr << shift) >> shift;
-	return immOp;
+	int shift1 = CSIZE + ISIZE + OP1SIZE + OP2SIZE;
+	int shift2 = OP1SIZE + OP2SIZE + OP3SIZE;
+	short int immOp;
+
+	if ((instr>>shift2) %2 == 1) {
+		immOp = (instr << shift1) >> shift1;
+		return immOp;
+	}
+	printf("Fatal error: In instruction %d, operand at immediate position is not an immediate\n", instr);
+	exit(0);
 }
 
 void setImmOp (InstructionType *instr, char *immStr) {
 	int shift1 = CSIZE + ISIZE + OP1SIZE + OP2SIZE;
-	int shift2 = ISIZE + OP1SIZE + OP2SIZE + OP3SIZE;
+	int shift2 = OP1SIZE + OP2SIZE + OP3SIZE;
 	InstructionType newInstr;
 	
 	short int immOp = (short int) atoi(immStr+1);	
 	newInstr = instr >> shift2;
-	newInstr += (1 << CSIZE);
+	newInstr += 1;
 	newInstr = newInstr << shift1;
 	newInstr += immOp;
 	*instr = newInstr;
